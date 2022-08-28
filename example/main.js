03294496883f0b788ba7953eb8f8acc2578a7a22
electrikmilk <brandonjordan124@gmail.com>
2022-08-28 14:50:19 -0400

    Improve example

--- example/main.js
+++ example/main.js
@@ -14,6 +14,7 @@ import {Dropdown} from 'jsUI';
 import {Line} from 'jsUI';
 import {ScrollView} from 'jsUI';
 import {select} from 'jsUI';
+import {Div} from 'jsUI';
 
 jsUI.addCSS({
 	'selector': {
@@ -21,11 +22,19 @@ jsUI.addCSS({
 	}
 });
 
-jsUI.globalStyle('input,textarea,select')
+jsUI.globalStyle('input,textarea,select,button')
+	.textSize('18px')
 	.rounded('5px')
 	.border('#c7c7c7', '2px')
 	.paddings(['8px', '5px'])
 	.font('system');
+
+jsUI.globalStyle('button')
+	.backgroundColor('dodgerblue')
+	.borderColor('dodgerblue')
+	.textColor('white')
+	.bold()
+	.paddings(['8px','15px'])
 
 jsUI.view([
 	Header([
@@ -44,7 +53,7 @@ jsUI.view([
 				.center()
 				.textColor('white')
 				.url('https://example.com')
-		], 5)
+		])
 	])
 		.textColor('white')
 		.backgroundColor('black'),
@@ -54,21 +63,19 @@ jsUI.view([
 		Text('Item'),
 		Text('Item'),
 		Text('Item')
-	], 5),
+	])
+		.border("gray"),
 	Section([
+		Button('Button')
+			.block(),
+		Spacer(),
 		Dropdown({
 			'1': 'Item 1',
 			'2': 'Item 2'
 		}).onChange((e) => {
 			console.log('changed!', e.target);
 		}),
-		Button('Button')
-			.onClick((e) => {
-				console.log('clicked!', e.target);
-				select('domo')
-					.rotate();
-			}),
-		Spacer(),
+		Spacer(2),
 		TextInput()
 			.block()
 			.placeholder('TextInput'),
@@ -90,14 +97,56 @@ jsUI.view([
 			.truncate(1)
 			.italic()
 			.bold(),
-		Image('domo.png')
-			.name('domo')
-			.lazyLoad()
-			.caption('Thank you.')
-			.size('300px', '300px')
-			.border('blue')
-			.shake()
-			.circle(),
+		Section([
+			Div([
+				Div([
+					Image('domo.png')
+						.name('domo')
+						.lazyLoad()
+						.caption('Thank you...')
+						.size('300px', '300px')
+						.border('blue')
+						.circle()
+				])
+					.center(),
+				Spacer(),
+				HStack([
+					Button("Rotate")
+						.onClick(() => {
+							select('domo')
+								.rotate()
+						}),
+					Button("Shake")
+						.onClick(() => {
+							select('domo')
+								.shake()
+						}),
+					Button("Flip")
+						.onClick(() => {
+							select('domo')
+								.flip()
+						}),
+					Button("Bounce")
+						.onClick(() => {
+							select('domo')
+								.bounce()
+						}),
+					Button("Fade In")
+						.onClick(() => {
+							select('domo')
+								.fadeIn()
+						}),
+					Button("Fade Out")
+						.onClick(() => {
+							select('domo')
+								.fadeOut()
+						}),
+				])
+					.gridGap('10px')
+			])
+		])
+			.center(),
+		Spacer(),
 		ScrollView([
 			GridStack([
 				Image('domo.png'),
@@ -109,7 +158,8 @@ jsUI.view([
 		])
 			.height('300px')
 	])
-])
+], true)
+	.textSize('18px')
 	.fontSmoothing()
 	.font('system')
 	.accent('#ab2c34');
