4c0fc7204b556c781152a61cf8e4f279be93bb91
Brandon Jordan <brandonjordan124@gmail.com>
2022-08-29 15:32:38 -0400

    Update style.js

--- src/style.js
+++ src/style.js
@@ -3,7 +3,7 @@
  */
 
 import {StandardAnimation} from './animations.js';
-import {addCSS} from './jsUI.js';
+import {addCSS, globalStyle} from './jsUI.js';
 
 export class Style extends StandardAnimation {
 	#element;
