d82b7d8e7aabd9d3f304f2384bae56306046a9c2
Brandon Jordan <brandonjordan124@gmail.com>
2023-07-16 17:06:56 -0400

    Add Base Element class

---
+++ src/base-element.js
@@ -0,0 +1,106 @@
+/*
+ * Copyright (c) 2023 Brandon Jordan
+ */
+
+export class BaseElement {
+    element;
+    mountedCallback;
+    createdCallback;
+    #inputTypes = ['text', 'search', 'url', 'number', 'password', 'email', 'tel'];
+
+    constructor(element) {
+        this.element = element;
+    }
+
+    tag() {
+        let tagName = null;
+        if (this.element && this.element.tagName) {
+            tagName = this.element.tagName.toLowerCase();
+        }
+        return tagName;
+    }
+
+    isTextInput() {
+        const tagname = this.tag();
+        return ((tagname === 'input' && this.#inputTypes.contains(this.element.type)) || tagname === 'textarea');
+    }
+
+    created(callback) {
+        this.createdCallback = callback;
+        return this;
+    }
+
+    mounted(callback) {
+        this.mountedCallback = callback;
+        return this;
+    }
+
+    attribute(attribute, value) {
+        this.element.setAttribute(attribute, value);
+        return this;
+    }
+
+    property(property, value) {
+        this.element[property] = value;
+        return this;
+    }
+
+    id(id) {
+        return this.attribute('id', id);
+    }
+
+    class(className) {
+        return this.attribute('class', className);
+    }
+
+    classes(classes) {
+        if (Array.isArray(classes)) {
+            this.element.className = classes.join(' ');
+        }
+        return this;
+    }
+
+    addClass(c) {
+        let classes = this.element.className.split(' ');
+        classes.push(c);
+        this.element.className = classes.join(' ');
+    }
+
+    removeClass(c) {
+        let classes = this.element.className.split(' ');
+        let classIndex = classes.indexOf(c);
+        classes = classes.splice(classIndex, 1);
+        this.element.className = classes.join(' ');
+    }
+
+    text(text) {
+        return this.property('innerText', text);
+    }
+
+    html(html) {
+        return this.property('innerHTML', html);
+    }
+
+    share(content) {
+        let data;
+        if (content.constructor && content.constructor === Object) {
+            data = content;
+        } else if (content.includes('http')) {
+            data = {
+                url: content
+            };
+        } else {
+            data = {
+                text: content
+            };
+        }
+        if (navigator.share && navigator.canShare(data)) {
+            navigator.share(data)
+                .then(() => console.log('[javascript-ui] Content was shared'))
+                .catch((error) => console.log('[javascript-ui] Error sharing content', error));
+        } else {
+            console.error('[javascript-ui] Unable to share!');
+        }
+        return this;
+    }
+}
