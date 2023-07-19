b6ad28f0f362a01d78522015a775c4924f3bd6eb
Brandon Jordan <brandonjordan124@gmail.com>
2023-07-19 19:24:19 -0400

    Improve Dialog() element

--- src/elements/Dialog.js
+++ src/elements/Dialog.js
@@ -4,15 +4,39 @@
 
 import {Element} from '../element.js';
 
-export function Dialog(elements) {
-    return new DialogTag(elements);
+export function Dialog(elements, open = null) {
+    return new DialogTag(elements, open);
 }
 
 class DialogTag extends Element {
-    constructor(elements) {
+    constructor(elements, open) {
         const element = document.createElement('dialog');
         super(element);
         this.element = element;
         this.elements = elements;
+        if (open) {
+            this.open(open);
+        }
+    }
+
+    open(state) {
+        return this.toggleAttribute('open', state);
+    }
+
+    show() {
+        this.element.showModal();
+    }
+
+    close(data) {
+        this.element.close(data);
+    }
+
+    onClose(callback) {
+        this.element.onclose = (event) => callback(this, event);
+        return this;
+    }
+
+    returnValue() {
+        return this.element.returnValue;
     }
 }
